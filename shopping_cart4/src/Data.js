2a56186cd93532cfbc735c29f07bc0ff55f4c07b
Kshitij s Pyage <128199725+ikay9ine@users.noreply.github.com>
2023-09-24 12:20:55 +0530

    Add files via upload

---
+++ shopping_cart4/src/Data.js
@@ -0,0 +1,24 @@
+let shopItemsData = [
+    {
+      id: "jfhg12122s",
+      name: "LAPTOP",
+      price: 1000,
+      desc: "Ed.init-laptop-color black",
+      img: "images/photo-1511385348-a52b4a160dc2.jpg",
+    },
+    {
+      id: "ioytrhn34324v",
+      name: "MOUSE",
+      price: 100,
+      desc: "Ed.init-mouse",
+      img: "images/mouse-alienware-aw320m-black-gallery-5.avif",
+    },
+    {
+      id: "wuefbncx328214n",
+      name: "HEADPHONE",
+      price: 50,
+      desc: "Ed.init-headphone",
+      img: "images/stock-photo--high-quality-headphones-on-a-white-background-headphone-product-photo-1574611990.jpg",
+    },
+    
+  ];
